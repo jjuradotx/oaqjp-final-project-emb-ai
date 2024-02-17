55c0bea28b30413025693f17ee8e0bb20ec25c25
JJurado <JJurado@gmail.com>
2024-02-17 11:17:41 -0500

    New saves

--- server.py
+++ server.py
@@ -10,11 +10,18 @@ app = Flask("Emotion Detector")
 def emot_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    return 'nice'
+    anger_value=response['anger']
+    disgust_value=response['disgust']
+    fear_value=response['fear']
+    joy_value=response['joy']
+    sadness_value=response['sadness']
+    dominant_value=response['dominant_emotion']
+    
+    return 'For the given statement, the system response is anger: {}, disgust: {}, fear: {}, joy: {} and sadness: {}. The dominant emotion is {}.'.format(anger_value, disgust_value, fear_value, joy_value, sadness_value, dominant_value)
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5005)
